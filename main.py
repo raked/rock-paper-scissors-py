622c8e4854b7c277f9f78fc3fba29e2c0f0dd49f
raked <raked@users.noreply.github.com>
2021-07-15 11:05:46 -0400

    Added randomness
    programmed for if the results are the same

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import random
 
 
 def main():
     print("Welcome to Rock Paper Scissors")
+    print("Select: (1)Rock (2)Paper (3)Scissors")
+    userChoice = input()
+    cpuChoice = random.randint(1, 3)
+    if (int(userChoice) == cpuChoice):
+        print("You both chose the same. Try again.")
 
 
 main()
