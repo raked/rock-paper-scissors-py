c05ee64dbb269bbf5a94c889cc7172ba78d8ddf7
raked <raked@users.noreply.github.com>
2021-07-15 11:14:56 -0400

    More cases

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ def main():
     cpuChoice = random.randint(1, 3)
     if (int(userChoice) == cpuChoice):
         print("You both chose the same. Try again.")
-    elif(int(userChoice) == 1 and cpuChoice == 2):
+    elif (int(userChoice) == 1 and cpuChoice == 2):
         print("Paper beats rock. CPU wins.")
-    elif(int(userChoice) == 1 and cpuChoice == 3):
+    elif (int(userChoice) == 1 and cpuChoice == 3):
         print("Rock beats scissors. You win.")
+    elif (int(userChoice) == 2 and cpuChoice == 1):
+        print("Paper beats rock. You win.")
+    elif (int(userChoice) == 2 and cpuChoice == 3):
+        print("Scissors beats paper. CPU wins.")
 
 
 main()
